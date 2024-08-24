3d85eebee77a4d462db5f219684177ebbf59b31e
SeguRuiz <ruizsegura96199@gmail.com>
2024-08-24 14:56:50 -0600

    base de datos y tablas creadas -Luis

--- crear-tablas-base/crear_tablas.sql
+++ crear-tablas-base/crear_tablas.sql
@@ -16,7 +16,7 @@ correo_electronico varchar(50) not null,
 telefono_personal varchar(30),
 primary key (id),
 constraint cliente_unico unique (id, nombre, apellidos)  
-)
+);
 
 -- Administradores 
 CREATE TABLE administradores (
@@ -26,7 +26,7 @@ apellidos varchar(100),
 correo_empresarial varchar(100) not null,
 primary key (id),
 constraint admin_unico unique (id, nombre, apellidos)
-)
+);
 
 
 
@@ -43,7 +43,7 @@ correo varchar(100) not null,
 primary key (id),
 foreign key (admin_id) references administradores(id) on delete cascade,
 constraint hotel_unico unique (id, ubicacion)
-) 
+); 
 
 -- tipos de habitaciones
 CREATE TABLE tipos_habitaciones (
@@ -52,7 +52,7 @@ nombre varchar(50) not null,
 limite_huespedes int not null,
 precio int not null,
 primary key (id)
-)
+);
 
 -- habitaciones
 CREATE TABLE habitaciones (
@@ -64,9 +64,51 @@ telefono varchar(30) not null,
 primary key (id),
 foreign key (hotel_id) references hoteles(id) on delete cascade,
 foreign key (tipo_id) references tipos_habitaciones(id) on delete cascade 
-)
+);
 
 
 
 -- Tablas-reservaciones-huespedes
 
+-- reservaciones
+CREATE TABLE reservaciones (
+id int auto_increment not null,
+cliente_id int not null,
+hotel_id int not null,
+fecha_entrada date not null,
+fecha_salida date not null,
+activa bool default true,
+primary key (id),
+foreign key (cliente_id) references clientes(id) on delete cascade,
+foreign key (hotel_id) references hoteles(id) on delete cascade,
+constraint reservacion_unica unique (id, cliente_id, fecha_entrada, fecha_salida)
+); 
+
+
+-- huespedes
+CREATE TABLE huespedes (
+id int auto_increment not null,
+hotel_id int not null,
+reservacion_id int not null,
+primary key (id),
+foreign key (hotel_id) references hoteles(id) on delete cascade,
+foreign key (reservacion_id) references reservaciones(id) on delete cascade
+);
+
+
+-- reservacion habitacion
+CREATE TABLE reservas_habitaciones (
+id int auto_increment not null,
+reservacion_id int not null,
+habitacion_id int not null,
+primary key (id),
+foreign key (reservacion_id) references reservaciones(id) on delete cascade,
+foreign key (habitacion_id) references habitaciones(id) on delete cascade
+);
+
+
+
+
+
+-- si es necesario 
+drop database reserva_hoteles;
