4e8c4efecf9624716685c4ba903397d9ef3abd8e
SeguRuiz <ruizsegura96199@gmail.com>
2024-08-28 09:45:34 -0600

    queries -luis

--- querys/querys_luis.sql
+++ querys/querys_luis.sql
@@ -1,2 +1,29 @@
 -- Luis 
+USE reserva_hoteles;
+
+-- ○ Consulta para buscar hoteles cuya ubicación termina con un texto específico.
+SELECT * FROM hoteles WHERE ubicacion LIKE '%Costa Rica';
+
+-- ○ Consulta para obtener las reservas de un cliente (por email) realizadas en el mes anterior.
+SELECT * FROM reservaciones 
+WHERE MONTH(fecha_agregacion) = MONTH(ADDDATE(CURDATE(), INTERVAL -1 MONTH)) 
+AND cliente_id IN (SELECT id FROM clientes WHERE correo_electronico = 'marcos.rodriguez@example.com');
+
+-- ○ Consulta para identificar el hotel con la mayor ocupación en el mes anterior.
+SELECT habitaciones_ocupacion.hotel_id, SUM(habitaciones_ocupacion.limite_huespedes) 
+AS maximo_ocupacion 
+FROM habitaciones_ocupacion
+WHERE nombre IS NOT NULL AND limite_huespedes IS NOT NULL 
+AND habitaciones_ocupacion.id IN (SELECT habitacion_id FROM habitaciones_reservadas_fechas WHERE ADDDATE(CURDATE(), INTERVAL -2 MONTH) BETWEEN fecha_entrada AND fecha_salida)
+GROUP BY
+hotel_id; 
+
+-- ○ Consulta para listar los hoteles que tienen habitaciones disponibles pero no han sido reservadas en el último mes.
+SELECT habitaciones.hotel_id AS 'hotel', habitaciones.id AS 'habitacion' FROM habitaciones WHERE habitaciones.id IN 
+(SELECT habitacion_id FROM habitaciones_reservadas_fechas WHERE estado_id = 3 AND id IN (SELECT id FROM reservaciones WHERE MONTH(fecha_agregacion) = MONTH(ADDDATE(CURDATE(), INTERVAL -1 MONTH))) 
+AND CURDATE())) OR
+habitaciones.id NOT IN (SELECT habitacion_id FROM reservas_habitaciones WHERE habitacion_id = habitaciones.id);
+
+-- ○ Consulta para calcular el promedio de reservas diarias en un hotel.
+SELECT AVG(numero_reservaciones) AS promedio_reservaciones_diarias FROM reservaciones_diarias;
 
--- vistas-reserva/vistas.sql
+++ vistas-reserva/vistas.sql
@@ -21,7 +21,7 @@ SELECT * FROM reservaciones_hoy;
 
 -- Vista que muestra las habitaciones ocupadas con sus fechas de reservacion
 CREATE VIEW habitaciones_reservadas_fechas AS 
-SELECT habitacion_id, reservaciones.cliente_id, reservaciones.fecha_entrada, reservaciones.fecha_salida, reservaciones.activa FROM 
+SELECT habitacion_id, reservaciones.id, reservaciones.cliente_id, reservaciones.fecha_entrada, reservaciones.fecha_salida, reservaciones.activa FROM 
 reservas_habitaciones LEFT JOIN 
 reservaciones ON 
 reservas_habitaciones.reservacion_id = reservaciones.id;  
@@ -29,3 +29,27 @@ reservas_habitaciones.reservacion_id = reservaciones.id;
 -- Para ver y eliminar la vista
 SELECT * FROM habitaciones_reservadas_fechas;
 DROP VIEW habitaciones_reservadas_fechas;
+
+
+-- Vista para ver la ocupacion del hotel
+CREATE VIEW habitaciones_ocupacion AS
+SELECT habitaciones.id, habitaciones.hotel_id, habitaciones.telefono, tipos_habitaciones.nombre, tipos_habitaciones.limite_huespedes FROM habitaciones
+LEFT JOIN
+tipos_habitaciones ON
+habitaciones.tipo_id = tipos_habitaciones.id AND habitaciones.id IN (SELECT habitacion_id FROM habitaciones_reservadas_fechas WHERE activa = TRUE)
+
+
+DROP VIEW habitaciones_ocupacion
+
+SELECT * FROM habitaciones_ocupacion;
+DROP VIEW habitaciones_ocupacion;
+
+-- ver las reservaciones diarias
+CREATE VIEW reservaciones_diarias AS 
+SELECT fecha_agregacion, COUNT(id) AS numero_reservaciones
+FROM reservaciones GROUP BY
+fecha_agregacion;
+
+DROP VIEW reservaciones_diarias
+
+SELECT * FROM reservaciones_diarias
